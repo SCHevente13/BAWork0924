d78810beb8d1f57975524e4aa9fe41e6d7f1d23e
Bereg.Levente <Bereg.Levente@C113-04>
2026-09-24 08:42:19 +0200

    Added Book.cs

---
+++ ConsoleApp5/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal record Book(string Title, string Author, int Pages)
+    {
+        public double Price { get; set; }
+        public void UpdatePrice(int percent)
+        {
+            Price *= (percent / 100) / 100;
+        }
+        public double GetPrice3() 
+        { 
+            return Price * 3; 
+        }
+
+    }
+}
