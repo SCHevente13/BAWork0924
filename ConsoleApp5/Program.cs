f1b9c5057f1864b9b2a7b415dff4c004ac769579
bereg.levente <bereg.levente@C116-02>
2026-09-25 13:16:17 +0200

    stuff

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Book b = new Book("title", "Author", 1);
+            Book b = new Book("The Hobbit", "J. R. R. Tolkien", 310) { Price = 6500 };
+            Book b = new Book("Dune", "Frank Herbert", 688) { Price = 7900 };
             Student s = new Student("name", "name", 1);
         }
     }
